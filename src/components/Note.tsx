0a4d248f6a6db61db6b95f6ce6388fbc6ea47403
iykat <princeiykat@gmail.com>
2024-01-14 11:38:30 +0000

    Update NoteInterface and note component to render dates

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,18 +1,20 @@
+import { formatDate } from "@/utils/helpers";
 import { FC } from "react";
-
-interface NoteProps {
+export interface NoteInterface {
   title: string;
-  body: string;
+  noteBody: string;
+  createdAt: Date,
+  updatedAt: Date
 }
 
-const Note: FC<NoteProps> = ({ title, body }) => {
+const Note: FC<NoteInterface> = ({ title, noteBody, createdAt, updatedAt }) => {
   return (
     <div className="mb-4 hover:bg-neutral-100 p-2 rounded-lg hover:cursor-pointer">
       <h2 className="overflow-ellipsis clamp-text font-semibold">{title}</h2>
-      <p className="opacity-90 text-sm overflow-ellipsis clamp-text">{body}</p>
-      <div className="flex text-xs opacity-80 mt-1">
-        <p className="mr-auto">Created at: some date</p>
-        <p>Updated at: some date</p>
+      <p className="opacity-90 text-sm overflow-ellipsis clamp-text">{noteBody}</p>
+      <div className="flex text-xs opacity-40 mt-1">
+        <p className="mr-auto">Created at: {formatDate(createdAt)}</p>
+        <p>Updated at: {formatDate(updatedAt)}</p>
       </div>
     </div>
   );
