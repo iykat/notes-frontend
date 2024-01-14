12cd44b199b40a161d7251b5f38e2a4428ad9b4b
iykat <princeiykat@gmail.com>
2024-01-14 08:21:33 +0000

    Initial setup and configuration

---
+++ src/components/Homepage.tsx
@@ -0,0 +1,7 @@
+export default function Homepage() {
+  return <main className="h-screen bg-red-100">
+    <div className="flex items-center justify-center  bg-neutral-400">
+    Notes
+    </div>
+  </main>
+}
